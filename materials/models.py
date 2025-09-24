62dcb65fbbbe97aa69bd80c3893b7aa1a329c70f
xziste <17alexdmz99@gmail.com>
2025-09-24 12:44:44 +0300

    add model Course in app materials

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField(
+        max_length=100, verbose_name="Название курса", help_text="Укажите название курса"
+    )
+    description = models.TextField(
+        verbose_name="Описание", help_text="Введите описание курса", blank=True, null=True
+    )
+    preview_image = models.ImageField(
+        upload_to='materials/preview_image/', blank=True, null=True, help_text='Загрузите превью курса'
+    )
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+        ordering = [
+            "name",
+        ]
+
+    def __str__(self):
+        return self.name
