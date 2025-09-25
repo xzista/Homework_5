29b3246e1413d8b9809017c42c105ced50793a1d
xziste <17alexdmz99@gmail.com>
2025-09-25 18:21:27 +0300

    add urls in app users

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import SimpleRouter
+
+from users.apps import UsersConfig
+from users.views import UserViewSet
+
+app_name = UsersConfig.name
+
+router = SimpleRouter()
+router.register('users', UserViewSet)
