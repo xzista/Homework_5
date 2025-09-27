e17ced762b9cfbcc05c95a10dffa560bd0596d8f
xziste <17alexdmz99@gmail.com>
2025-09-27 20:05:44 +0300

    add users and payments in admin page

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User, Payment
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "email",
+        "phone",
+        "city",
+        "avatar",
+        "is_active",
+    )
+    list_filter = (
+        "email",
+        "city",
+        "is_active",
+    )
+    search_fields = ("email", "city", "phone",)
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "data",
+        "content_type",
+        "object_id",
+        "paid_object",
+        "amount",
+        "payment_method",
+    )
+    list_filter = (
+        "user",
+        "amount",
+        "payment_method",
+    )
+    search_fields = ("user",)
