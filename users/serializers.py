580c9437ce69d709cc0aeb5e62ee7199f52c3454
xziste <17alexdmz99@gmail.com>
2025-09-28 16:18:08 +0300

    add PaymentSerializer in app users

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework.serializers import ModelSerializer
 
-from users.models import User
+from users.models import User, Payment
+
+
+class PaymentSerializer(ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = '__all__'
 
 
 class UserSerializer(ModelSerializer):
--- users/views.py
+++ users/views.py
@@ -1,9 +1,16 @@
 from rest_framework.viewsets import ModelViewSet
 
-from users.models import User
-from users.serializers import UserSerializer
+from users.models import User, Payment
+from users.serializers import UserSerializer, PaymentSerializer
 
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class PaymentViewSet(ModelViewSet):
+    queryset = Payment.objects.all()
+
+    def get_serializer_class(self):
+        return PaymentSerializer
