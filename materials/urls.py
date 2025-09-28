514f3f4e6511077471ebc374c3b0db2b2367fa89
xziste <17alexdmz99@gmail.com>
2025-09-28 15:48:03 +0300

    small fix in urls

--- materials/urls.py
+++ materials/urls.py
@@ -13,7 +13,7 @@ router.register("courses", CourseViewSet)
 
 urlpatterns = [
     path("lessons/", LessonListApiView.as_view(), name="lessons_list"),
-    path("lessons/<int:pk>", LessonRetrieveApiView.as_view(), name="lessons_retrieve"),
+    path("lessons/<int:pk>/", LessonRetrieveApiView.as_view(), name="lessons_retrieve"),
     path("lessons/create/", LessonsCreateApiView.as_view(), name="lessons_create"),
     path("lessons/<int:pk>/delete/", LessonDestroyApiView.as_view(), name="lessons_delete"),
     path("lessons/<int:pk>/update/", LessonUpdateApiView.as_view(), name="lessons_update"),
