387d8917f5e80ac68fc12063017ecd0132754e6b
xziste <17alexdmz99@gmail.com>
2025-10-01 19:54:14 +0300

    add permissions for unauthenticated users

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -12,7 +13,7 @@ router.register("payments", PaymentViewSet)
 
 urlpatterns = [
     path('register/', UserCreateAPIView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('login/', TokenObtainPairView.as_view(permissions_classes=(AllowAny,)), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
 from users.models import Payment, User
@@ -10,6 +11,7 @@ from users.serializers import PaymentFilter, PaymentSerializer, UserSerializer
 class UserCreateAPIView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
 
     def perform_create(self, serializer):
         user = serializer.save(is_active=True)
