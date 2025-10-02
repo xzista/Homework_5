5253ea7b72640ca99e403799a759cda6b998563f
xziste <17alexdmz99@gmail.com>
2025-10-03 02:38:06 +0300

    add permission IsOwner in permissions.py in users

--- users/permissions.py
+++ users/permissions.py
@@ -2,8 +2,23 @@ from rest_framework import permissions
 
 
 class IsModer(permissions.BasePermission):
+    """
+    Проверяет, является ли пользователь модератором.
+    """
     message = 'Отказано в доступе'
 
     def has_permission(self, request, view):
         return request.user.is_authenticated and \
             request.user.groups.filter(name='Модераторы').exists()
+
+
+class IsOwner(permissions.BasePermission):
+    """
+    Проверяет, является ли пользователь владельцем.
+    """
+    message = 'Отказано в доступе'
+
+    def has_object_permission(self, request, view, obj):
+        if obj.owner == request.user:
+            return True
+        return False
