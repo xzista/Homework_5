a33ee82aa80f3d67e43f073cdf4b743584646efe
xziste <17alexdmz99@gmail.com>
2025-10-08 17:36:39 +0300

    adding tests for courses

--- materials/tests.py
+++ materials/tests.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 from materials.models import Course, Lesson
-from users.models import User
+from users.models import User, Subscription
 
 
 class LessonTestCase(APITestCase):
@@ -70,9 +70,6 @@ class LessonTestCase(APITestCase):
         url = reverse('materials:lessons_list')
         response = self.client.get(url)
         data = response.json()
-        self.assertEqual(
-            response.status_code, status.HTTP_200_OK
-        )
         result = {
             'count': 1,
             'next': None,
@@ -89,6 +86,96 @@ class LessonTestCase(APITestCase):
                 }
             ]
         }
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+            data, result
+        )
+
+
+class CourseTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(email='test@test.com')
+        self.course = Course.objects.create(name='testCourse1', owner=self.user)
+        self.lesson = Lesson.objects.create(
+            name='testLesson1',
+            url_video='test_url/youtube.com/test',
+            course=self.course,
+            owner=self.user
+        )
+        self.subscription = Subscription.objects.create(
+            user=self.user,
+            course=self.course,
+            is_active=True
+        )
+        self.client.force_authenticate(user=self.user)
+
+    def test_course_retrieve(self):
+        url = reverse('materials:course-detail', args=(self.course.pk,))
+        response = self.client.get(url)
+        data = response.json()
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+            data.get('name'), self.course.name
+        )
+
+    def test_course_create(self):
+        url = reverse('materials:course-list')
+        data = {
+            'name': 'testCourse2'
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(
+            response.status_code, status.HTTP_201_CREATED
+        )
+        self.assertEqual(
+            Course.objects.all().count(), 2
+        )
+
+    def test_course_update(self):
+        url = reverse('materials:course-detail', args=(self.course.pk,))
+        data = {
+            'name': 'testCourse1update',
+        }
+        response = self.client.patch(url, data)
+        data = response.json()
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+            data.get('name'), 'testCourse1update'
+        )
+
+    def test_course_delete(self):
+        url = reverse('materials:course-detail', args=(self.course.pk,))
+        response = self.client.delete(url)
+        self.assertEqual(
+            response.status_code, status.HTTP_204_NO_CONTENT
+        )
+        self.assertEqual(
+            Course.objects.all().count(), 0
+        )
+
+    def test_course_list(self):
+        url = reverse('materials:course-list')
+        response = self.client.get(url)
+        data = response.json()
+        print(data)
+        result = [{
+            'id': self.course.pk,
+            'is_subscribed': True,
+            'name': self.course.name,
+            'description': None,
+            'preview_image': None,
+            'owner': self.user.pk
+        }]
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
         self.assertEqual(
             data, result
         )
