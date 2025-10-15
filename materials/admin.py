d8f3c85b711180aad1975b1e4d45ecd8bfdb7cd2
xziste <17alexdmz99@gmail.com>
2025-10-15 04:37:21 +0300

    adding materials models in admin.py

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
+from materials.models import Course, Lesson
+
+
+@admin.register(Course)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "description",
+        "owner",
+        "price",
+        "preview_image",
+    )
+    list_filter = (
+        "owner",
+    )
+    search_fields = ("name", "description",)
+
+@admin.register(Lesson)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "description",
+        "course",
+        "url_video",
+        "owner",
+        "price",
+        "preview_image",
+    )
+    list_filter = (
+        "owner",
+        "course",
+    )
+    search_fields = ("name", "description",)
+
