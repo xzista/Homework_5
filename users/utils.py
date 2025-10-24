225a6b50bf199109bd3b91a6b79a20d07321a691
xziste <17alexdmz99@gmail.com>
2025-10-24 17:16:26 +0300

    adding func should_send_notification inside utils.py in app users

---
+++ users/utils.py
@@ -0,0 +1,17 @@
+from django.utils import timezone
+from datetime import timedelta
+
+
+def should_send_notification(course, lesson=None):
+    """
+    Проверяет, нужно ли отправлять уведомление об обновлении
+    (курс не обновлялся более 4 часов)
+    """
+    if lesson and lesson.course != course:
+        return False
+
+    if hasattr(course, 'updated_at'):
+        time_since_update = timezone.now() - course.updated_at
+        return time_since_update > timedelta(hours=4)
+
+    return True
